84207b9649fe269da34359bf009d23bccc5b0dc1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-02-28 14:31:45 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,11 +25,11 @@ class PostsController < ApplicationController
 
 	def edit
 	  @post = Post.find(params[:id])
-	end 
-	
+	end
+
 private
- 
- 
+
+
 	# We pass the permitted fields in as *args;
 	# this keeps `post_params` pretty dry while
 	# still allowing slightly different behavior
